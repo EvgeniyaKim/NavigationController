8be5d3702d9c346186f1f19bd59ce52a94e08c2b
student <student@hilton1.ttknn.net>
2018-02-01 16:23:30 +0300

    Добавила 3 файла

---
+++ NavigationController/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  NavigationController
+//
+//  Created by student on 01.02.2018.
+//  Copyright © 2018 Kim. All rights reserved.
+//
+
+import Foundation
---
+++ NavigationController/ViewController2.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController2.swift
+//  NavigationController
+//
+//  Created by student on 01.02.2018.
+//  Copyright © 2018 Kim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
